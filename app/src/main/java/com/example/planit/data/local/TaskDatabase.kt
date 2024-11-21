7edd8445a00a8049f23c5344ba04028ff2292be3
mvpamansingh <amancodes5@gmail.com>
2024-11-21 18:21:53 +0530

    Implemented and designed TaskEntity , TaskDao , and Room Database class

--- app/src/main/java/com/example/planit/data/local/TaskDatabase.kt
+++ app/src/main/java/com/example/planit/data/local/TaskDatabase.kt
@@ -4,6 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.planit.data.local.dao.Converters
+import com.example.planit.data.local.dao.TaskDao
 import com.example.planit.data.local.entity.TaskEntity
 
 @Database(entities = [TaskEntity::class], version = 1)
